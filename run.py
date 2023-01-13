b1c84d02ad628f27986356e0a74f67858ee7e92d
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2023-01-13 10:24:44 +0000

    Update run.py

--- run.py
+++ run.py
@@ -6,12 +6,14 @@ from citycatio.inputs import GreenAreas
 # move files to output dir
 result_data_dir = '/data/inputs'
 output_data_dir = '/data/outputs'
+buildings_data_dir = '/data/inputs/buildings'
+green_areas_data_dir = '/data/inputs/green_areas'
 
 # make output dir if not exists
 os.makedirs(output_data_dir, exist_ok=True)
 
 # Reading in the buildings shapefile
-gdf = gpd.read_file(os.path.join(result_data_dir,'all_buildings.shp'))
+gdf = gpd.read_file(os.path.join(buildings_data_dir,'all_buildings.shp'))
 Buildings(gdf).write(output_data_dir)
 
 # Just printing the output of the file to show what's in it
@@ -19,7 +21,7 @@ Buildings(gdf).write(output_data_dir)
 #  print(*f.readlines()[:50])
 
 # Reading in the greenspaces shapefile
-gdf = gpd.read_file(os.path.join(result_data_dir,'all_greenareas.shp'))
+gdf = gpd.read_file(os.path.join(green_areas_data_dir,'all_greenareas.shp'))
 GreenAreas(gdf).write(output_data_dir)
 
 # Just printing the output of the file to show what's in it
